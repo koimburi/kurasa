23b812d58ff60f82d211f1315f77b0c5fa419d5e
koimburi <kahura@elewa.ke>
2024-12-23 12:39:58 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './contacts/components/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/components/contact-detail/contact-detail.component';
+import { ContactEditComponent } from './contacts/components/contact-edit/contact-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+    { path: 'contacts', component: ContactListComponent },
+    { path: 'contacts/:id', component: ContactDetailComponent },
+    { path: 'contacts/:id/edit', component: ContactEditComponent },
+];
