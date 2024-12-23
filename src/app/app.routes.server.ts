20ff9239754630b3db5946f21e3ff875db04da8e
koimburi <kahura@elewa.ke>
2024-12-23 12:55:03 +0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,25 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { mockContacts } from './contacts/utils/mock-contacts';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'contacts/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return mockContacts.map(contact => ({
+        id: contact.id
+      }));
+    }
+  },
+  {
+    path: 'contacts/:id/edit',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return mockContacts.map(contact => ({
+        id: contact.id
+      }));
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
